2f0a0bf99e80c932d9504d4222a405e21c8e1b44
jennellew <jennelle.wy.wong@gmail.com>
2012-07-09 20:10:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PbSkeleton::Application.config.secret_token = 'd3e95cbdaa1b9927046e648661f64523d3d4f7f7bfb40303db5be9206284d9e6fc05af299da73ac09b4ac0bf5b71fe8b1cdba5a8d11f3f191d6b76ebe874ee3f'
